ce028c7d36f51c13fb1851ea37625c75f5fdcad8
jpalmer37 <john.palmer1288@gmail.com>
2024-03-15 11:30:02 -0700

    small fix to report2.py

--- bin/report2.py
+++ bin/report2.py
@@ -64,7 +64,8 @@ def parse_blast(filepath):
 	df = pd.read_csv(filepath)
 
 	#df['database'] = os.path.basename(filepath).split('_')[1]
-	df["species"] = df["species"].apply(lambda x: "" if x == "a" else x)
+
+	df["species"] = df["species"].apply(lambda x: "" if x == "a" else x).astype(str)
 	df = df.sort_values(['query_seq_id', 'bitscore'], ascending=[True, False])
 
 	df['percent_identity'] = df['percent_identity'].round(3)
