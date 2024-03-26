7499b6c3a9222d312213b87c9ca9d6301d14520f
jpalmer37 <john.palmer1288@gmail.com>
2024-03-26 12:22:36 -0700

    minor edit to filtering in report

--- bin/report2.py
+++ bin/report2.py
@@ -66,7 +66,7 @@ def parse_blast(filepath):
 	#df['database'] = os.path.basename(filepath).split('_')[1]
 
 	df["species"] = df["species"].apply(lambda x: "" if x == "a" else x).astype(str)
-	df = df.sort_values(['percent_identity', 'bitscore'], ascending=[False, False])
+	df = df.sort_values(['percent_coverage', 'percent_identity'], ascending=[False, False])
 
 	df['percent_identity'] = df['percent_identity'].round(3)
 	df['percent_coverage'] = df['percent_coverage'].round(3)
